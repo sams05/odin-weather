b71d1bb34c2f00672ac9e7f63d6b30175c5cbacc
Samuel Shen <sshen05@terpmail.umd.edu>
2023-12-03 17:29:53 -0500

    Complete weather code mapping

--- script.js
+++ script.js
@@ -21,6 +21,7 @@ async function getWeatherData(latitude = 38.8951, longitude = -77.0364) {
 */
 
 // Cached data to test rendering data
+
 async function getWeatherData() {
     return {
         "time": {
@@ -54,15 +55,6 @@ async function getWeatherData() {
                 "2023-12-06"
             ],
             "weatherCode": [
-                0,
-                1,
-                2,
-                3,
-                0,
-                1,
-                2
-            ],
-            /*"weatherCode": [
                 3,
                 51,
                 3,
@@ -70,7 +62,7 @@ async function getWeatherData() {
                 53,
                 51,
                 73
-            ],*/
+            ],
             "temperatureLow": [
                 28.1,
                 33.5,
@@ -112,6 +104,8 @@ async function getWeatherData() {
 }
 
 // |Assets
+// Adapted from https://gist.github.com/stellasphere/9490c195ed2b53c707087c8c2db4ec0c
+// |Todo add LICENSE file for google fonts https://fonts.google.com/icons?icon.query=weather
 const WEATHER_CODE_MAPPING = {
     0: {
         day: {
@@ -161,6 +155,30 @@ const WEATHER_CODE_MAPPING = {
             iconFont: 'cloud'
         }
     },
+    45: {
+        day: {
+            description: 'Foggy',
+            image: 'https://images.pexels.com/photos/226460/pexels-photo-226460.jpeg',
+            iconFont: 'foggy'
+        },
+        night: {
+            description: 'Foggy',
+            image: 'https://images.pexels.com/photos/6494669/pexels-photo-6494669.jpeg',
+            iconFont: 'foggy'
+        }
+    },
+    48: {
+        day: {
+            description: 'Foggy',
+            image: 'https://images.pexels.com/photos/226460/pexels-photo-226460.jpeg',
+            iconFont: 'foggy'
+        },
+        night: {
+            description: 'Foggy',
+            image: 'https://images.pexels.com/photos/6494669/pexels-photo-6494669.jpeg',
+            iconFont: 'foggy'
+        }
+    },
     51: {
         day: {
             description: 'Light Drizzle',
@@ -173,6 +191,258 @@ const WEATHER_CODE_MAPPING = {
             iconFont: 'rainy'
         }
     },
+    53: {
+        day: {
+            description: 'Moderate Drizzle',
+            image: 'https://images.pexels.com/photos/3178798/pexels-photo-3178798.jpeg',
+            iconFont: 'rainy'
+        },
+        night: {
+            description: 'Moderate Drizzle',
+            image: 'https://images.pexels.com/photos/1824270/pexels-photo-1824270.jpeg',
+            iconFont: 'rainy'
+        }
+    },
+    55: {
+        day: {
+            description: 'Heavy Drizzle',
+            image: 'https://images.pexels.com/photos/3178798/pexels-photo-3178798.jpeg',
+            iconFont: 'rainy'
+        },
+        night: {
+            description: 'Heavy Drizzle',
+            image: 'https://images.pexels.com/photos/1824270/pexels-photo-1824270.jpeg',
+            iconFont: 'rainy'
+        }
+    },
+    56: {
+        day: {
+            description: 'Light Freezing Drizzle',
+            image: 'https://images.pexels.com/photos/3178798/pexels-photo-3178798.jpeg',
+            iconFont: 'weather_mix'
+        },
+        night: {
+            description: 'Light Freezing Drizzle',
+            image: 'https://images.pexels.com/photos/1824270/pexels-photo-1824270.jpeg',
+            iconFont: 'weather_mix'
+        }
+    },
+    57: {
+        day: {
+            description: 'Freezing Drizzle',
+            image: 'https://images.pexels.com/photos/3178798/pexels-photo-3178798.jpeg',
+            iconFont: 'weather_mix'
+        },
+        night: {
+            description: 'Freezing Drizzle',
+            image: 'https://images.pexels.com/photos/1824270/pexels-photo-1824270.jpeg',
+            iconFont: 'weather_mix'
+        }
+    },
+    61: {
+        day: {
+            description: 'Light Rain',
+            image: 'https://images.pexels.com/photos/3178798/pexels-photo-3178798.jpeg',
+            iconFont: 'rainy'
+        },
+        night: {
+            description: 'Light Rain',
+            image: 'https://images.pexels.com/photos/1824270/pexels-photo-1824270.jpeg',
+            iconFont: 'rainy'
+        }
+    },
+    63: {
+        day: {
+            description: 'Rain',
+            image: 'https://images.pexels.com/photos/3178798/pexels-photo-3178798.jpeg',
+            iconFont: 'rainy'
+        },
+        night: {
+            description: 'Rain',
+            image: 'https://images.pexels.com/photos/1824270/pexels-photo-1824270.jpeg',
+            iconFont: 'rainy'
+        }
+    },
+    65: {
+        day: {
+            description: 'Heavy Rain',
+            image: 'https://images.pexels.com/photos/3178798/pexels-photo-3178798.jpeg',
+            iconFont: 'rainy'
+        },
+        night: {
+            description: 'Heavy Rain',
+            image: 'https://images.pexels.com/photos/1824270/pexels-photo-1824270.jpeg',
+            iconFont: 'rainy'
+        }
+    },
+    66: {
+        day: {
+            description: 'Light Freezing Rain',
+            image: 'https://images.pexels.com/photos/3178798/pexels-photo-3178798.jpeg',
+            iconFont: 'weather_mix'
+        },
+        night: {
+            description: 'Light Freezing Rain',
+            image: 'https://images.pexels.com/photos/1824270/pexels-photo-1824270.jpeg',
+            iconFont: 'weather_mix'
+        }
+    },
+    67: {
+        day: {
+            description: 'Freezing Rain',
+            image: 'https://images.pexels.com/photos/3178798/pexels-photo-3178798.jpeg',
+            iconFont: 'weather_mix'
+        },
+        night: {
+            description: 'Freezing Rain',
+            image: 'https://images.pexels.com/photos/1824270/pexels-photo-1824270.jpeg',
+            iconFont: 'weather_mix'
+        }
+    },
+    71: {
+        day: {
+            description: 'Light Snow',
+            image: 'https://images.pexels.com/photos/9215186/pexels-photo-9215186.jpeg',
+            iconFont: 'cloudy_snowing'
+        },
+        night: {
+            description: 'Light Snow',
+            image: 'https://images.pexels.com/photos/8698973/pexels-photo-8698973.jpeg',
+            iconFont: 'cloudy_snowing'
+        }
+    },
+    73: {
+        day: {
+            description: 'Snow',
+            image: 'https://images.pexels.com/photos/9215186/pexels-photo-9215186.jpeg',
+            iconFont: 'cloudy_snowing'
+        },
+        night: {
+            description: 'Snow',
+            image: 'https://images.pexels.com/photos/8698973/pexels-photo-8698973.jpeg',
+            iconFont: 'cloudy_snowing'
+        }
+    },
+    75: {
+        day: {
+            description: 'Heavy Snow',
+            image: 'https://images.pexels.com/photos/9215186/pexels-photo-9215186.jpeg',
+            iconFont: 'cloudy_snowing'
+        },
+        night: {
+            description: 'Heavy Snow',
+            image: 'https://images.pexels.com/photos/8698973/pexels-photo-8698973.jpeg',
+            iconFont: 'cloudy_snowing'
+        }
+    },
+    77: {
+        day: {
+            description: 'Snow Grains',
+            image: 'https://images.pexels.com/photos/9215186/pexels-photo-9215186.jpeg',
+            iconFont: 'cloudy_snowing'
+        },
+        night: {
+            description: 'Snow Grains',
+            image: 'https://images.pexels.com/photos/8698973/pexels-photo-8698973.jpeg',
+            iconFont: 'cloudy_snowing'
+        }
+    },
+    80: {
+        day: {
+            description: 'Light Showers',
+            image: 'https://images.pexels.com/photos/3178798/pexels-photo-3178798.jpeg',
+            iconFont: 'rainy'
+        },
+        night: {
+            description: 'Light Showers',
+            image: 'https://images.pexels.com/photos/1824270/pexels-photo-1824270.jpeg',
+            iconFont: 'rainy'
+        }
+    },
+    81: {
+        day: {
+            description: 'Showers',
+            image: 'https://images.pexels.com/photos/3178798/pexels-photo-3178798.jpeg',
+            iconFont: 'rainy'
+        },
+        night: {
+            description: 'Showers',
+            image: 'https://images.pexels.com/photos/1824270/pexels-photo-1824270.jpeg',
+            iconFont: 'rainy'
+        }
+    },
+    82: {
+        day: {
+            description: 'Heavy Showers',
+            image: 'https://images.pexels.com/photos/3178798/pexels-photo-3178798.jpeg',
+            iconFont: 'rainy'
+        },
+        night: {
+            description: 'Heavy Showers',
+            image: 'https://images.pexels.com/photos/1824270/pexels-photo-1824270.jpeg',
+            iconFont: 'rainy'
+        }
+    },
+    85: {
+        day: {
+            description: 'Light Snow Showers',
+            image: 'https://images.pexels.com/photos/9215186/pexels-photo-9215186.jpeg',
+            iconFont: 'cloudy_snowing'
+        },
+        night: {
+            description: 'Light Snow Showers',
+            image: 'https://images.pexels.com/photos/8698973/pexels-photo-8698973.jpeg',
+            iconFont: 'cloudy_snowing'
+        }
+    },
+    86: {
+        day: {
+            description: 'Snow Showers',
+            image: 'https://images.pexels.com/photos/9215186/pexels-photo-9215186.jpeg',
+            iconFont: 'cloudy_snowing'
+        },
+        night: {
+            description: 'Snow Showers',
+            image: 'https://images.pexels.com/photos/8698973/pexels-photo-8698973.jpeg',
+            iconFont: 'cloudy_snowing'
+        }
+    },
+    95: {
+        day: {
+            description: 'Thunderstorm',
+            image: 'https://images.pexels.com/photos/1162251/pexels-photo-1162251.jpeg',
+            iconFont: 'thunderstorm'
+        },
+        night: {
+            description: 'Thunderstorm',
+            image: 'https://images.pexels.com/photos/1162251/pexels-photo-1162251.jpeg',
+            iconFont: 'thunderstorm'
+        }
+    },
+    96: {
+        day: {
+            description: 'Thunderstorm With Hail',
+            image: 'https://images.pexels.com/photos/1162251/pexels-photo-1162251.jpeg',
+            iconFont: 'weather_hail'
+        },
+        night: {
+            description: 'Thunderstorm With Hail',
+            image: 'https://images.pexels.com/photos/1162251/pexels-photo-1162251.jpeg',
+            iconFont: 'weather_hail'
+        }
+    },
+    99: {
+        day: {
+            description: 'Thunderstorm With Hail',
+            image: 'https://images.pexels.com/photos/1162251/pexels-photo-1162251.jpeg',
+            iconFont: 'weather_hail'
+        },
+        night: {
+            description: 'Thunderstorm With Hail',
+            image: 'https://images.pexels.com/photos/1162251/pexels-photo-1162251.jpeg',
+            iconFont: 'weather_hail'
+        }
+    }
 }
 
 // |Render
@@ -187,7 +457,7 @@ const CURRENT_CONDITION_SECTION = {
 
 function interpretWeatherCode(weatherCode) {
     // |todo use time to differentiate between night and day
-    const dayNight = 'night';
+    const dayNight = 'day';
     const { description, image, iconFont } = WEATHER_CODE_MAPPING[weatherCode][dayNight];
     return { description, image, iconFont };
 }
