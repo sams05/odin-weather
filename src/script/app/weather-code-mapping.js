d4690e37bcca5dae2551496f97c48acc6606ebe4
Samuel Shen <sshen05@terpmail.umd.edu>
2023-12-14 00:13:09 -0500

    Add attributions

--- src/script/app/weather-code-mapping.js
+++ src/script/app/weather-code-mapping.js
@@ -1,6 +1,5 @@
 
 // Adapted from https://gist.github.com/stellasphere/9490c195ed2b53c707087c8c2db4ec0c
-// |Todo add LICENSE file for google fonts https://fonts.google.com/icons?icon.query=weather
 const WEATHER_CODE_MAPPING = {
     0: {
         day: {
