a64f35220f9f2bd59e8b5ee8438d46b6f9836049
Samuel Shen <sshen05@terpmail.umd.edu>
2023-12-18 11:23:17 -0500

    Add favicon

--- webpack.common.js
+++ webpack.common.js
@@ -1,8 +1,22 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 
 module.exports = {
     entry: './src/script/script.js',
     plugins: [
+        new FaviconsWebpackPlugin({
+            logo: './src/favicon/sunny.png',
+            favicons: {
+                icons: {
+                    android: false,
+                    appleIcon: false,
+                    appleStartup: false,
+                    favicons: true,
+                    windows: false,
+                    yandex: false,    
+                },       
+            },
+        }),
         new HtmlWebpackPlugin({
             template: './src/index.html',
         }),
