6e42bf378bf26b4409bf21fd1f4888e13c87886a
Samuel Shen <sshen05@terpmail.umd.edu>
2023-12-19 09:24:09 -0500

    Publish docs folder

--- webpack.prod.js
+++ webpack.prod.js
@@ -9,7 +9,7 @@ module.exports = merge(common, {
     mode: 'production',
     output: {
         filename: 'main.[contenthash].js',
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'docs'),
         clean: true,
     },
     plugins: [
